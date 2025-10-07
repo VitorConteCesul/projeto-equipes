416cf16f0f2a5ac138ca8cddaeb7252136b49273
VitorConteCesul <vitor.2024014338@alunocesul.com>
2025-10-06 22:36:08 -0300

    ThemeProvider

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,11 @@
+import { Container} from "@screens/Teams/styles";
+import { LoadIndicator } from "./styles";
+
+export function Loading() {
+  
+  return (
+    <Container>
+      <LoadIndicator />
+    </Container>
+  )
+}
