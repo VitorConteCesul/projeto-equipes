1dc7336cd42ebf6ae64f291ce85b2cd29d1150db
VitorConteCesul <vitor.2024014338@alunocesul.com>
2025-11-10 22:38:22 -0300

    feat: criando MemberCard

---
+++ src/components/MemberCard/index.tsx
@@ -0,0 +1,25 @@
+import { ButtonIcon } from '@components/ButtonIcon';
+
+import { Container, Name } from './styles';
+
+type Props = {
+  name: string;
+  onRemove: () => void;
+}
+
+export function MemberCard({ name, onRemove }: Props) {
+  return(
+    <Container>
+      <Name>
+        {name}
+      </Name>
+
+      <ButtonIcon 
+        icon="delete-outline" 
+        type="SECONDARY"
+        size='SM'
+        onPress={onRemove}
+      />
+    </Container>
+  );
+}
